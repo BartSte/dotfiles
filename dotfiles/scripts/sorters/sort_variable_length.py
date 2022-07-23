036d3ef938ffbe6632c1987dd22eb32856e45a5d
barts <bartsteensma@outlook.com>
2022-07-23 10:53:42 +0200

    Update python

--- dotfiles/scripts/sorters/sort_variable_length.py
+++ dotfiles/scripts/sorters/sort_variable_length.py
@@ -1,5 +1,21 @@
 from sys import stdin
 
+
+def sort_variable_length(lines: str) -> str:
+    """Sorts lines of variables definitions base on the number of characters
+    before the `=' sign.
+
+    Args:
+        lines (str): variable definitions separated by `\n'.
+
+    Returns:
+        str: sorted variable definitions separated by `\n'.
+    """
+    as_list = lines.splitlines()
+    as_list.sort(key=find_equal_sign)
+    return '\n'.join(as_list)
+
+
 def find_equal_sign(obj: str) -> int:
     """Returns the index of the = sign.
 
@@ -13,8 +29,6 @@ def find_equal_sign(obj: str) -> int:
 
 
 if __name__ == '__main__':
-    lines = stdin.read().splitlines()
-    lines.sort(key=find_equal_sign)
-    stdout = '\n'.join(lines)
+    lines = stdin.read()
+    stdout = sort_variable_length(lines)
     print(stdout)
-
