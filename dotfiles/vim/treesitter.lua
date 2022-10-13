5cbce40f7453e6dd90759961177f006f89559551
Bart Steensma <b.steensma@fleetcleaner.com>
2022-10-13 12:28:58 +0200

    improved treesitter

--- dotfiles/vim/treesitter.lua
+++ dotfiles/vim/treesitter.lua
@@ -27,9 +27,9 @@ require'nvim-treesitter.configs'.setup {
         enable = true,
         keymaps = {
             init_selection = "gnn",
-            node_incremental = "grn",
-            scope_incremental = "grc",
-            node_decremental = "grm",
+            node_incremental = "gnn",
+            scope_incremental = "gni",
+            node_decremental = "gnd",
         },
     },
 }
