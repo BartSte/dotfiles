c5d3d7c74b834baf7705c7b7ce50eba86292bd31
barts <b.steensma@fleetcleaner.com>
2022-11-02 14:03:49 +0100

    improve ftplugin python

--- dotfiles/nvim/ftplugin/lua.lua
+++ dotfiles/nvim/ftplugin/lua.lua
@@ -1,6 +1,6 @@
 --Work around for bug in LSP -> all marks are deleted after format.
 local function format()
-    vim.lsp.buf.format { async = true }
+    vim.lsp.buf.format({ async = true })
 end
 
 local opts = { buffer = true }
--- dotfiles/nvim/ftplugin/python.lua
+++ dotfiles/nvim/ftplugin/python.lua
@@ -1,6 +1,10 @@
 local executable = vim.fn['executable']
 
-if executable('autopep8') then
+if executable('autopep8') == 1 then
     vim.bo.formatprg = 'autopep8 -'
+else
+    local opts = { buffer = true }
+    local message = '<cmd>echo "No autopep8 installed"<CR>'
+    vim.keymap.set('n', 'gq', message, opts)
+    vim.keymap.set('n', '<space>f', message, opts)
 end
-
