487d75b81d86ff2822388dbe4c16e42ba563e361
barts <bartsteensma@outlook.com>
2022-11-19 10:39:35 +0100

    Enabled arduino-cli and atreus

--- dotfiles/nvim/lua/conf-lsp-servers.lua
+++ dotfiles/nvim/lua/conf-lsp-servers.lua
@@ -7,6 +7,16 @@ lspconfig.jsonls.setup({})
 lspconfig.clangd.setup({})
 lspconfig.cssls.setup({})
 
+lspconfig.arduino_language_server.setup {
+    cmd = {
+        "arduino-language-server",
+        "-cli-config", "~/.arduino15/arduino-cli.yaml",
+        "-fqbn", "keyboardio:avr:keyboardio_atreus",
+        "-cli", "arduino-cli",
+        "-clangd", "clangd"
+    }
+}
+
 lspconfig.sumneko_lua.setup({
     single_file_support = true,
     flags = { debounce_text_changes = 150 },
