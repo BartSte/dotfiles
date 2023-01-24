c353d4b261c77f46121da234ac396050a446c1e1
barts <b.steensma@fleetcleaner.com>
2023-01-24 15:56:36 +0100

    'update conceal'

--- dotfiles/nvim/after/queries/python/highlights.scm
+++ dotfiles/nvim/after/queries/python/highlights.scm
@@ -1,9 +1,10 @@
 ; extends
 
-("->" @operator (set! conceal "➔"))
+("->" @operator (set! conceal "→"))
 ("==" @operator (set! conceal "≡"))
 ("!=" @operator (set! conceal "≢"))
 ("<=" @operator (set! conceal "≤"))
 (">=" @operator (set! conceal "≥"))
-("*" @operator (set! conceal "∙"))
-("/" @operator (set! conceal "÷"))
+("lambda" @operator (set! conceal "λ"))
+; ("*" @operator (set! conceal "∙"))
+; ("/" @operator (set! conceal "÷"))
