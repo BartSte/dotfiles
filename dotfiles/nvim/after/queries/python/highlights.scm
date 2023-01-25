a10018b7e1fa8e57a5b45cf4d44102ba5d32140b
barts <b.steensma@fleetcleaner.com>
2023-01-25 10:03:57 +0100

    'added highlighs.scm for lua'

--- dotfiles/nvim/after/queries/python/highlights.scm
+++ dotfiles/nvim/after/queries/python/highlights.scm
@@ -1,10 +1,11 @@
 ; extends
 
-("->" @operator (set! conceal "→"))
+; ("->" @operator (set! conceal "→"))
+("->" @operator (set! conceal ""))
 ("==" @operator (set! conceal "≡"))
-("!=" @operator (set! conceal "≢"))
+("!=" @operator (set! conceal "≠"))
 ("<=" @operator (set! conceal "≤"))
 (">=" @operator (set! conceal "≥"))
-("lambda" @operator (set! conceal "λ"))
-; ("*" @operator (set! conceal "∙"))
-; ("/" @operator (set! conceal "÷"))
+("lambda" @keyword.lambda (set! conceal "λ"))
+
+; ((type (identifier) @type) (#eq? @type "float") (set! conceal "ℝ"))
