d2abd84593f01fc56392370310f6e1d6760b5749
barts <bartsteensma@outlook.com>
2023-03-08 21:20:39 +0100

    ''

--- dotfiles/nvim/ftplugin/org.lua
+++ dotfiles/nvim/ftplugin/org.lua
@@ -1,6 +1,3 @@
 local keymapper = require('keymapper')
 
 vim.opt_local.conceallevel = 2
-
-keymapper.noremap('<leader>r', ':silent !calsync &<CR>')
-
--- dotfiles/nvim/ftplugin/orgagenda.lua
+++ dotfiles/nvim/ftplugin/orgagenda.lua
@@ -2,6 +2,3 @@ local keymapper = require('keymapper')
 
 vim.opt_local.conceallevel = 2
 
-keymapper.noremap('<leader>r', ':silent !calsync &<CR>')
-
-
