7b6ef76fe1db3659ffa6bae55a4aa2cec90b7f57
barts <b.steensma@fleetcleaner.com>
2023-03-20 08:43:03 +0100

    ''

--- dotfiles/nvim/ftplugin/org.lua
+++ dotfiles/nvim/ftplugin/org.lua
@@ -1,3 +1,5 @@
 local keymapper = require('keymapper')
 
 vim.opt_local.conceallevel = 2
+
+keymapper.nnoremap('<leader>r', ':silent !mycalsync<CR>')
--- dotfiles/nvim/ftplugin/orgagenda.lua
+++ dotfiles/nvim/ftplugin/orgagenda.lua
@@ -2,3 +2,4 @@ local keymapper = require('keymapper')
 
 vim.opt_local.conceallevel = 2
 
+keymapper.nnoremap('<leader>r', ':silent !mycalsync<CR>')
