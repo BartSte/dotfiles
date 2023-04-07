dcf9b33f604dbf59107019b5cfe0804b4cd7cf43
bartsteensma <b.steensma@fleetcleaner.com>
2023-04-07 17:59:50 +0200

    update

--- dotfiles/nvim/ftplugin/python.lua
+++ dotfiles/nvim/ftplugin/python.lua
@@ -1,1 +1,3 @@
 vim.opt_local.conceallevel = 2
+vim.opt_local.colorcolumn = '79'
+vim.opt_local.textwidth = 80
--- dotfiles/nvim/lua/conf-lsp-servers.lua
+++ dotfiles/nvim/lua/conf-lsp-servers.lua
@@ -44,3 +44,11 @@ lspconfig.lua_ls.setup {
     },
   },
 }
+
+--Enable (broadcasting) snippet capability for completion
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+require'lspconfig'.html.setup {
+  capabilities = capabilities,
+}
