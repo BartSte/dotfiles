343e8149dc7fdc41c8e66a73c35d3d5e64f9cbf5
barts <bartsteensma@outlook.com>
2023-04-12 11:34:06 +0200

    ''

--- dotfiles/nvim/ftplugin/org.lua
+++ dotfiles/nvim/ftplugin/org.lua
@@ -1,5 +1,4 @@
-local keymapper = require('keymapper')
-
 vim.opt_local.conceallevel = 2
-
-keymapper.nnoremap('<leader>r', ':silent !mycalsync<CR>')
+vim.opt_local.spell = true
+vim.opt_local.textwidth = 79
+vim.opt_local.colorcolumn = '79'
--- dotfiles/nvim/lua/config-nvim-tree.lua
+++ dotfiles/nvim/lua/config-nvim-tree.lua
@@ -2,29 +2,35 @@
 vim.g.loaded = 1
 vim.g.loaded_netrwPlugin = 0
 
-local keymapper = require('keymapper')
 local tree = require('nvim-tree')
+local keymapper = require('keymapper')
 
 local my_mappings = {
-    { key = "u", action = "dir_up" },
+    { key = "u",    action = "dir_up" },
     { key = "<BS>", action = "" },
     { key = "<CR>", action = "" },
-    { key = "x", action = "close_node" },
+    { key = "x",    action = "close_node" },
 }
 
 tree.setup({
     sort_by = "name",
     disable_netrw = true,
-      hijack_cursor = true,
-      sync_root_with_cwd = true,
+    hijack_cursor = true,
+    sync_root_with_cwd = true,
     view = {
-        adaptive_size = true,
+        width = { max = 30, padding = 0 },
         mappings = { list = my_mappings, },
         relativenumber = true,
+        signcolumn = "no"
     },
-    renderer = { group_empty = true, },
-    filters = { dotfiles = false, },
-    git = { ignore = false }
+    renderer = { group_empty = true },
+    filters = { dotfiles = false },
+    git = { ignore = false },
+    experimental = {
+        git = {
+            async = true,
+        }
+    }
 })
 
 keymapper.noremap('<a-X>', '<cmd>NvimTreeFindFile<CR>')
