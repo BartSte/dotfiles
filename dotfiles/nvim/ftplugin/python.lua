05485c7c7d22ea0ee2b551aadbbd14a915499bdd
barts <bartsteensma@outlook.com>
2023-04-29 11:57:42 +0200

    ''

--- dotfiles/nvim/ftplugin/python.lua
+++ dotfiles/nvim/ftplugin/python.lua
@@ -13,3 +13,6 @@ local opts = {
 vim.keymap.set('n', '<a-C>', function() dappy.test_class(opts) end)
 vim.keymap.set('n', '<a-T>', function() dappy.test_method(opts) end)
 vim.keymap.set('n', '<a-S>', function() dappy.test_selection(opts) end)
+
+
+-- require("vim.lsp.utils").format { timeout_ms = 2000 }
