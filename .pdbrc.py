bce54d76e4b44a74f1f7ba173fe3206c0619d684
barts <b.steensma@fleetcleaner.com>
2023-05-16 21:33:09 +0200

    ''

---
+++ .pdbrc.py
@@ -0,0 +1,9 @@
+
+from pdb import DefaultConfig
+
+class Config(DefaultConfig):
+    prompt = '(Pdb++) '
+    highlight = True
+    colorscheme = 'neutral'
+    use_pygments = True
+    sticky_by_default = True
