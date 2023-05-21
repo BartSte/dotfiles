7c9f79d0909f677158abf8520dfe39669689d442
barts <bartsteensma@outlook.com>
2023-05-21 19:11:06 +0200

    ''

--- dotfiles/nvim/lua/config-treesitter.lua
+++ dotfiles/nvim/lua/config-treesitter.lua
@@ -1,21 +1,15 @@
 require 'nvim-treesitter.install'.compilers = { "cl", "gcc", "clang" }
 
 ---@diagnostic disable-next-line: unused-local
-local function disable_highlight(lang, buf)
-    local max_filesize = 100 * 1024 -- 100 KB
-    local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-    if ok and stats and stats.size > max_filesize then
-        return true
-    else
-        return { 'org', 'orgagenda' }
-    end
-end
-
-local my_highlight = {
-    enable = true,
-    disable = disable_highlight,
-    additional_vim_regex_highlighting = { 'org', 'orgagenda' }
-}
+-- local function disable_highlight(lang, buf)
+--     local max_filesize = 100 * 1024 -- 100 KB
+--     local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+--     if ok and stats and stats.size > max_filesize then
+--         return true
+--     else
+--         return { 'org', 'orgagenda' }
+--     end
+-- end
 
 require 'nvim-treesitter.configs'.setup {
     ensure_installed = {
@@ -25,7 +19,11 @@ require 'nvim-treesitter.configs'.setup {
     sync_install = false,
     auto_install = true,
     ignore_install = {},
-    highlight = my_highlight,
+    highlight = {
+        enable = true,
+        disable = { 'org', 'orgagenda' },
+        additional_vim_regex_highlighting = { 'org', 'orgagenda' }
+    },
     indent = { enable = true }
 }
 
