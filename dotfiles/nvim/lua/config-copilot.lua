9da674bf1428532fda7ee5a636bc67f7c525aad1
barts <b.steensma@fleetcleaner.com>
2023-06-12 09:54:03 +0200

    ''

--- dotfiles/nvim/lua/config-copilot.lua
+++ dotfiles/nvim/lua/config-copilot.lua
@@ -1,29 +1,26 @@
 local copilot = require("copilot")
+local suggestion = require("copilot.suggestion")
+local keymapper = require("keymapper")
+
+local function is_at_end_of_line()
+    return vim.fn.col(".") == vim.fn.col("$")
+end
+
+-- Execute `func` if the cursor is at the end of the line, after this and feed the `key`.
+local function execute_at_eol(callback, key)
+    if is_at_end_of_line() then
+        callback()
+    end
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), "n", true)
+end
 
 copilot.setup({
-    panel = {
-        enabled = false,
-        auto_refresh = false,
-        keymap = {
-            jump_prev = "[[",
-            jump_next = "]]",
-            accept = "<CR>",
-            refresh = "gr",
-            open = "<M-CR>"
-        },
-        layout = {
-            position = "right",     -- | top | left | right
-            ratio = 0.4
-        },
-    },
+    panel = { enabled = false },
     suggestion = {
         enabled = true,
         auto_trigger = true,
         debounce = 75,
         keymap = {
-            accept = "<M-i>",
-            dismiss = "<M-m>",
-            accept_word = "<M-I>",
             accept_line = false,
             next = "<C-n>",
             prev = "<C-p>",
@@ -41,6 +38,10 @@ copilot.setup({
         mail = true,
         ["*"] = false,
     },
-    copilot_node_command = 'node',     -- Node.js version must be > 16.x
+    copilot_node_command = 'node', -- Node.js version must be > 16.x
     server_opts_overrides = {},
 })
+
+keymapper.inoremap("<Right>", function() execute_at_eol(suggestion.accept, "<Right>") end)
+keymapper.inoremap("<Left>", function() execute_at_eol(suggestion.dismiss, "<Left>") end)
+keymapper.inoremap("<C-Right>", function() execute_at_eol(suggestion.accept_word, "<C-Right>") end)
