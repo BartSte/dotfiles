4dc9e9ad28ac044bbfdcea351a70c96d3aa9f187
barts <bartsteensma@outlook.com>
2023-06-17 22:19:55 +0200

    ''

--- dotfiles/nvim/lua/plugins/navigation.lua
+++ dotfiles/nvim/lua/plugins/navigation.lua
@@ -1,8 +1,23 @@
 return {
-    { 'junegunn/fzf',     build = ':call fzf#install()' },
-    { 'junegunn/fzf.vim', dependencies = { 'junegunn/fzf' } },
     'moll/vim-bbye',
     'BartSte/hop.nvim',
+    {
+        'junegunn/fzf.vim',
+        keys = {
+            { '<a-o>' },
+            { '<a-v>' },
+            { '<a-r>' },
+            { '<a-c>' },
+            { '<a-A>' }
+        },
+        cmd = { 'Ag' },
+        config = function()
+            vim.cmd('runtime vim/fzf.vim')
+        end,
+        dependencies = {
+            { 'junegunn/fzf', build = ':call fzf#install()' },
+        }
+    },
     {
         'stevearc/oil.nvim',
         event = 'TermOpen', -- when fzf is triggered
