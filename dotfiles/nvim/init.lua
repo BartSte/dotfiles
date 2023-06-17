cae39ef8ac6444bba9616ec058767e611a54e1c3
barts <bartsteensma@outlook.com>
2023-06-17 10:19:45 +0200

    ''

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -1,7 +1,3 @@
-vim.cmd('runtime! vim/before/*.vim')
-
 require('helpers.lazy').bootstrap()
 require('lazy').setup('plugins', {  change_detection = { enabled = false }})
 require("config")
-
-vim.cmd('runtime! vim/after/*')
