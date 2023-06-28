5af9f611b72f6ae370e6240401584ad60fb86a5c
barts <bartsteensma@outlook.com>
2023-06-28 11:11:54 +0200

    ''

--- dotfiles/nvim/lua/config/null-ls.lua
+++ dotfiles/nvim/lua/config/null-ls.lua
@@ -5,7 +5,9 @@ null_ls.setup({
         null_ls.builtins.formatting.autopep8,
         null_ls.builtins.formatting.ruff,
         null_ls.builtins.diagnostics.ruff,
-        null_ls.builtins.formatting.dprint,
-        null_ls.builtins.formatting.beautysh,
+        null_ls.builtins.formatting.shfmt.with({
+            extra_args = { "-i", "4", "-ci" },
+            filetypes = { "sh", "zsh", "bash" },
+        })
     },
 })
