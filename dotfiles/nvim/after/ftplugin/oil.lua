8e558dabd27ff7ee0e93c7af0c3cebd6800ac461
barts <bartsteensma@outlook.com>
2023-08-23 12:46:54 +0200

    ''

--- dotfiles/nvim/after/ftplugin/oil.lua
+++ dotfiles/nvim/after/ftplugin/oil.lua
@@ -1,7 +1,3 @@
-local helpers = require('helpers.oil')
-local keymapper = require('helpers.keymapper')
 vim.b.auto_save = 0
 vim.opt_local.colorcolumn = '79'
 vim.opt_local.textwidth = 79
-
-keymapper.buffer_noremap("<leader>r", helpers.rename)
