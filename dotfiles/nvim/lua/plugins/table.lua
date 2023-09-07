85e8837a87039b2ffdb88233cb9cd9ed65fe164e
barts <b.steensma@fleetcleaner.com>
2023-09-07 20:57:06 +0200

    ''

--- dotfiles/nvim/lua/plugins/table.lua
+++ dotfiles/nvim/lua/plugins/table.lua
@@ -1,7 +1,7 @@
 return {
     'dhruvasagar/vim-table-mode',
     ft = { 'markdown', 'org' },
-    command = { 'TableModeToggle', 'TableModeEnable' }, 
+    cmd = { 'TableModeToggle', 'TableModeEnable' },
     config = function()
         vim.cmd('runtime vim/table.vim')
     end
