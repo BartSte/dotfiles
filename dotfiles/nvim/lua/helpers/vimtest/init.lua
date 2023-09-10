42d3a2720030471f07581491d26ac55127bae385
barts <b.steensma@fleetcleaner.com>
2023-09-10 16:21:21 +0200

    ''

--- dotfiles/nvim/lua/helpers/vimtest/init.lua
+++ dotfiles/nvim/lua/helpers/vimtest/init.lua
@@ -4,13 +4,13 @@ local M = {}
 
 local default_opts = {
     pytest = {
-        loglevel = 'INFO',
-        wslwinvenvs = vim.fn.expand('$WH/venvs/'), -- FIX: not a good default
         test = {
+            loglevel = 'INFO',
             prefix = 'tmux clearhist; clear;',
             args = '-s -rA',
         },
         debug = {
+            loglevel = 'DEBUG',
             prefix = 'tmux clearhist; clear;',
             args = '-s -rA --pdb --pdbcls=IPython.terminal.debugger:Pdb',
         }
@@ -20,18 +20,18 @@ local default_opts = {
 M.setup = function(opts)
     opts = vim.tbl_deep_extend('force', default_opts, opts or {})
 
-    local pytest_opts = {
-        loglevel = opts.pytest.loglevel,
-        wslwinvenvs = opts.pytest.wslwinvenvs,
-        prefix = opts.pytest.test.prefix,
-        args = opts.pytest.test.args,
-    }
+    local test = opts.pytest.test
+    local debug = opts.pytest.debug
 
+    local pytest_opts = {
+        loglevel = test.loglevel,
+        prefix = test.prefix,
+        args = test.args,
+    }
     local pytest_opts_debug = {
-        loglevel = opts.pytest.loglevel,
-        wslwinvenvs = opts.pytest.wslwinvenvs,
-        prefix = opts.pytest.debug.prefix,
-        args = opts.pytest.debug.args,
+        loglevel = debug.loglevel,
+        prefix = debug.prefix,
+        args = debug.args,
     }
 
     M.pytest = {
--- dotfiles/nvim/lua/helpers/vimtest/pytest.lua
+++ dotfiles/nvim/lua/helpers/vimtest/pytest.lua
@@ -2,43 +2,48 @@ local tslime = require('helpers.tslime')
 
 local M = {}
 
--- When running WSL and a venv is present in g:wsl_win_venvs, it will take
--- presedence over $VIRTUAL_ENV.
-local function get_venv(wsl_win_venvs)
-    local directory = vim.fn.fnamemodify(vim.fn.getcwd(), ':t')
-    local wsl_win_venv = wsl_win_venvs .. directory
-    if vim.fn.isdirectory(wsl_win_venv) == 1 then
-        return wsl_win_venv
-    elseif vim.env.VIRTUAL_ENV ~= nil then
-        return vim.env.VIRTUAL_ENV
+-- Returns the output of `wpy -p` if it exists, otherwise returns ''.
+local function wsl_win_py()
+    if vim.fn.executable('wpy') == 1 then
+        local stdout = vim.fn.system('wpy -p')
+        return string.gsub(stdout, '\n$', '') -- Remove trailing newline
     else
         return ''
     end
 end
 
-local function get_venv_dev()
-    local wsl_win_venv = vim.fn.system('wpy -p')
+-- Returns the path to the python executable in the Windows venv.
+local function win_py()
+    return vim.env.VIRTUAL_ENV .. '\\Scripts\\python.exe'
+end
+
+-- Returns the path to the python executable in the Linux venv.
+local function linux_py()
+    return vim.env.VIRTUAL_ENV .. '/bin/python'
+end
+
+-- When running WSL and a venv is returned by `wpy -p`, use that venv. If not,
+-- return the $VIRTUAL_ENV that corresponds to the OS.
+local function get_py()
+    local wsl_win_venv = wsl_win_py()
     if wsl_win_venv ~= '' then
         return wsl_win_venv
-    elseif vim.env.VIRTUAL_ENV ~= nil then
-        return vim.env.VIRTUAL_ENV
-    else
+    elseif vim.env.VIRTUAL_ENV == nil then
         return ''
+    elseif vim.fn.has('win32') == 1 then
+        return win_py()
+    else
+        return linux_py()
     end
 end
 
--- Retruns the test command that corresponds to the WSL, WSL+windows, or linux
--- venv.
-local function get_command(venv, cmd)
-    if venv == '' then
+-- Replace the first occurence of `pattern` in `cmd` with `replacement`. If
+-- `replacement` is '' or nil, return `cmd` unchanged.
+local function replace(cmd, pattern, replacement)
+    if replacement == '' or replacement == nil then
         return cmd
-    elseif vim.fn.has('win32') == 1 then
-        return venv .. '\\Scripts\\' .. cmd
-    elseif venv == vim.env.VIRTUAL_ENV then
-        return venv .. '/bin/' .. cmd
     else
-        local python_exe_cmd = string.gsub(cmd, '^python[^ ]+ ', 'python.exe ')
-        return venv .. '/Scripts/' .. python_exe_cmd
+        return string.gsub(cmd, pattern, replacement, 1)
     end
 end
 
@@ -51,17 +56,18 @@ local default_opts = {
 
 -- Test strategy that can run tests for Linux projects and Windows projects
 -- that are edited from WSL. `args` is a string of additional arguments to pass
--- to pytest.
--- `command` will be runned before the test command. For example, if you want
--- to clear the screen before running the tests, you can pass `clear;` as
--- command.
+-- to pytest. `command` will be runned before the test command. For example, if
+-- you want to clear the screen before running the tests, you can pass `clear;`
+-- as command.
 M.make_strategy = function(opts)
     opts = vim.tbl_deep_extend('force', default_opts, opts or {})
     return function(cmd)
-        local venv = get_venv(opts.wslwinvenvs)
-        local full_cmd = opts.prefix .. get_command(venv, cmd)
         local args = opts.args .. ' --log-level=' .. opts.loglevel
-        tslime.send_to_tmux(full_cmd .. ' ' .. args .. '\n')
+
+        cmd = replace(cmd, "^python[^ ]+", get_py())
+        cmd = opts.prefix .. ' ' .. cmd .. ' ' .. args
+
+        tslime.send_to_tmux(cmd .. '\n')
     end
 end
 
