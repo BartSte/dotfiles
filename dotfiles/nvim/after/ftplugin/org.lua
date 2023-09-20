b9c86ee940d077a7d209d51c75faa971b53c4962
barts <bartsteensma@outlook.com>
2023-09-20 12:03:56 +0200

    ''

--- dotfiles/nvim/after/ftplugin/org.lua
+++ dotfiles/nvim/after/ftplugin/org.lua
@@ -3,5 +3,6 @@ vim.opt_local.shiftwidth = 2
 vim.opt_local.softtabstop = 2
 vim.opt_local.textwidth = 0
 vim.opt_local.colorcolumn = ''
+vim.opt_local.conceallevel = 2
 
 require('helpers.keymapper').nnoremap('<leader>oie', '<cmd>Email<CR>')
