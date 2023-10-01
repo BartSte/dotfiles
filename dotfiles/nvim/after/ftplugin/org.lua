fee802f451de21e08838d71d00a7f7efb785ca54
barts <bartsteensma@outlook.com>
2023-10-01 16:08:34 +0200

    ''

--- dotfiles/nvim/after/ftplugin/org.lua
+++ dotfiles/nvim/after/ftplugin/org.lua
@@ -1,7 +1,7 @@
 vim.opt_local.spell = true
 vim.opt_local.shiftwidth = 2
 vim.opt_local.softtabstop = 2
-vim.opt_local.textwidth = 0
+vim.opt_local.textwidth = 100
 vim.opt_local.colorcolumn = ''
 vim.opt_local.conceallevel = 2
 
