4af234ce8e875de67a6bb30c89db28a7a559f3d9
barts <bartsteensma@outlook.com>
2023-10-10 21:42:40 +0200

    ''

--- dotfiles/nvim/lua/plugins/null-ls.lua
+++ dotfiles/nvim/lua/plugins/null-ls.lua
@@ -1,5 +1,5 @@
 return {
-    'jose-elias-alvarez/null-ls.nvim',
+    'nvimtools/none-ls.nvim',
     ft = { 'python', 'zsh', 'sh', 'bash', 'markdown', 'c', 'cpp', 'h', 'hpp', 'cmake' },
     event = { "BufReadPost", "BufNewFile" },
     config = function()
