adfe152ca9f4ab76d99938bf2c098c3c31d6c425
barts <bartsteensma@outlook.com>
2023-12-07 20:59:15 +0100

    ''

--- dotfiles/nvim/after/ftplugin/org.lua
+++ dotfiles/nvim/after/ftplugin/org.lua
@@ -1,4 +1,5 @@
 local keymapper = require('helpers.keymapper')
+
 vim.opt_local.spell = true
 vim.opt_local.shiftwidth = 2
 vim.opt_local.softtabstop = 2
@@ -6,6 +7,4 @@ vim.opt_local.textwidth = 100
 vim.opt_local.colorcolumn = ''
 vim.opt_local.conceallevel = 2
 
-
 keymapper.nnoremap('<leader>oie', '<cmd>Email<CR>')
-keymapper.nnoremap('ZZ', '<cmd>lua require("orgmode").action("capture.refile")<CR>')
