1b6e2cb795435bcb595324294312358bae2729ee
barts <bartsteensma@outlook.com>
2023-12-07 17:31:36 +0100

    ''

--- dotfiles/nvim/lua/plugins/org.lua
+++ dotfiles/nvim/lua/plugins/org.lua
@@ -6,7 +6,8 @@ return {
     ft = { 'org' },
     keys = {
         { "<a-r>" },
-        { "<space>oa", "<Cmd>lua require('orgmode').action('agenda.prompt')<CR>" }
+        { "<space>oa", "<Cmd>lua require('orgmode').action('agenda.prompt')<CR>" },
+        { "<space>oc", "<Cmd>lua require('orgmode').action('capture.prompt')<CR>" },
     },
     cond = function() return vim.fn.has('linux') == 1 end,
     dependencies = {
