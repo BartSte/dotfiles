12a32afefebf2d45e2ef6f31af41c44200f67969
barts <bartsteensma@outlook.com>
2024-02-20 20:54:55 +0100

    'Automatic Update'

--- dotfiles/nvim/lua/plugins/hop.lua
+++ dotfiles/nvim/lua/plugins/hop.lua
@@ -1,5 +1,8 @@
 return {
     'BartSte/hop.nvim',
+    -- 'smoka/hop.nvim', -- maintained fork of original
+    -- It is not compatible yet as they changed module that containts the
+    -- `regex_by_search` function that I uced in the helpers.
     event = { "BufReadPost", "BufNewFile" },
     config = function()
         require('config.hop')
