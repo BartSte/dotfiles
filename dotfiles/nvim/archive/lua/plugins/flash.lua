f32f3d3a5ba762cfb89190d42eabddeabe22a5ca
barts <bartsteensma@outlook.com>
2024-02-21 11:08:04 +0100

    ''

--- dotfiles/nvim/archive/lua/plugins/flash.lua
+++ dotfiles/nvim/archive/lua/plugins/flash.lua
@@ -1,3 +1,5 @@
+-- I did not like flash as it took lots of effort to mimic the behavior of
+-- hop.nvim (displaying multiple labels only when needed).
 return {
     "folke/flash.nvim",
     event = "VeryLazy",
--- dotfiles/nvim/lua/plugins/hop.lua
+++ dotfiles/nvim/lua/plugins/hop.lua
@@ -1,9 +1,6 @@
 return {
     'smoka7/hop.nvim',
     event = { "BufReadPost", "BufNewFile" },
-    cond = function()
-        return false
-    end,
     config = function()
         require('config.hop')
     end
