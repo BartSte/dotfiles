76e842d4f0bdb8d188b0a3e4b9f08e5a84f61243
barts <bartsteensma@outlook.com>
2024-02-27 22:24:48 +0100

    'Automatic Update'

--- dotfiles/nvim/after/ftplugin/python.lua
+++ dotfiles/nvim/after/ftplugin/python.lua
@@ -20,7 +20,14 @@ keymapper.buffer_nnoremap('<a-T>', ':TestNearest -strategy=pytesttmuxdebug<CR>')
 vim.opt_local.formatprg = 'autopep8 -'
 
 -- map leader + f to format the entire file. This can be done using gggqG, but
--- we need to make sure the cursor is returned to the original position. This
--- can be done by jumping two times back in the jump list.
-keymapper.buffer_nnoremap('<leader><leader>f', "mxgggqG'x")
-
+-- we need to make sure the cursor is returned its original position.
+local function format_file()
+    local cursor = vim.fn.getpos('.')
+    if cursor == nil then
+        vim.notify('Cursor position not found', vim.log.levels.ERROR)
+    else
+        vim.cmd('normal! gggqG')
+        vim.fn.setpos('.', cursor)
+    end
+end
+keymapper.buffer_nnoremap('<leader>f', format_file)
