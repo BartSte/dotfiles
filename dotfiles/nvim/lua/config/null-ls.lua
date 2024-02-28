db185dae8c1bcbf2d7592c8eca428c3c73e6bed6
barts <bartsteensma@outlook.com>
2024-02-28 09:12:33 +0100

    'Automatic Update'

--- dotfiles/nvim/lua/config/null-ls.lua
+++ dotfiles/nvim/lua/config/null-ls.lua
@@ -1,38 +1,35 @@
-local null_ls = require("null-ls")
-
 local M = {}
 
-M.sources = {
-    -- Python
-    null_ls.builtins.diagnostics.pylint,
-
-    -- C++
-    null_ls.builtins.formatting.cmake_format,
-    null_ls.builtins.diagnostics.cmake_lint,
-
-    -- Bash/zsh
-    null_ls.builtins.formatting.shfmt.with({
-        extra_args = { "-i", "4", "-ci" },
-        filetypes = { "sh", "zsh", "bash" },
-    }),
-
-    -- Markdown
-    null_ls.builtins.formatting.prettier.with({
-        filetypes = { "markdown" },
-    }),
-    null_ls.builtins.diagnostics.markdownlint,
-}
-
-M.sources_extra = {
-    require("none-ls.formatting.autopep8")
-}
-
 M.setup = function()
-    null_ls.setup({ sources = M.sources })
+    local null_ls = require("null-ls")
+    null_ls.setup({
+        sources = {
+            -- Python
+            null_ls.builtins.diagnostics.pylint,
+
+            -- C++
+            null_ls.builtins.formatting.cmake_format,
+            null_ls.builtins.diagnostics.cmake_lint,
+
+            -- Bash/zsh
+            null_ls.builtins.formatting.shfmt.with({
+                extra_args = { "-i", "4", "-ci" },
+                filetypes = { "sh", "zsh", "bash" },
+            }),
+
+            -- Markdown
+            null_ls.builtins.formatting.prettier.with({
+                filetypes = { "markdown" },
+            }),
+            null_ls.builtins.diagnostics.markdownlint,
+        }
+    })
 end
 
 M.setup_extra = function()
-    null_ls.setup({ sources = M.sources_extra })
+    require("null-ls").setup({
+        sources = require("none-ls.formatting.autopep8")
+    })
 end
 
 return M
--- dotfiles/nvim/lua/plugins/null-ls.lua
+++ dotfiles/nvim/lua/plugins/null-ls.lua
@@ -1,23 +1,21 @@
+local function setup_mason()
+    require("mason-null-ls").setup({ automatic_installation = true })
+end
+
 return {
     "nvimtools/none-ls.nvim",
     ft = { "python", "zsh", "sh", "bash", "markdown", "c", "cpp", "h", "hpp", "cmake" },
     event = { "BufReadPost", "BufNewFile" },
-    config = function()
-        require("config.null-ls").setup()
-    end,
+    config = require("config.null-ls").setup,
     dependencies = {
-        {
-            "nvimtools/none-ls-extras.nvim",
-            config = function()
-                require("config.null-ls").setup_extra()
-            end
-        },
         "williamboman/mason.nvim",
         {
             "jay-babu/mason-null-ls.nvim",
-            config = function()
-                require("mason-null-ls").setup({ automatic_installation = true })
-            end
+            config = setup_mason
+        },
+        {
+            "nvimtools/none-ls-extras.nvim",
+            config = require("config.null-ls").setup_extra
         },
     }
 }
