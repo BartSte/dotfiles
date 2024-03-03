1546c63e3f416188c1dd7fcf5cb2b0389023eda9
barts <bartsteensma@outlook.com>
2024-03-03 11:44:34 +0100

    'Automatic Update'

--- dotfiles/nvim/lua/after/plugin/sorters.lua
+++ dotfiles/nvim/lua/after/plugin/sorters.lua
@@ -2,8 +2,11 @@ local map = require("helpers.keymapper")
 local M = {}
 
 M.setup = function()
-    map.nnoremap("<leader>c=", "vip:!lensort '='")
-    map.vnoremap("<leader>c=", ":!lensort '='<CR>")
+    map.nnoremap("<leader>ce", "vip:!lensort '='<CR>")
+    map.vnoremap("<leader>ce", ":!lensort '='<CR>")
+
+    map.nnoremap("<leader>c:", "vip:!lensort ': '<CR>")
+    map.vnoremap("<leader>c:", ":!lensort ': '<CR>")
 end
 
 return M
