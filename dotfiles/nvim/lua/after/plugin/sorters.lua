3a0237687daa78bfd6511f935a84cf25b621200b
barts <bartsteensma@outlook.com>
2024-03-24 11:41:20 +0100

    'Automatic Update'

--- dotfiles/nvim/lua/after/plugin/sorters.lua
+++ dotfiles/nvim/lua/after/plugin/sorters.lua
@@ -3,8 +3,8 @@ local map = require("helpers.keymapper")
 map.nnoremap("<leader>cc", "vip:sort<CR>")
 map.vnoremap("<leader>cc", ":sort<CR>")
 
-map.nnoremap("<leader>ce", "vip:!lensort '='<CR>")
-map.vnoremap("<leader>ce", ":!lensort '='<CR>")
+map.nnoremap("<leader>c=", "vip:!lensort '='<CR>")
+map.vnoremap("<leader>c=", ":!lensort '='<CR>")
 
 map.nnoremap("<leader>c:", "vip:!lensort ': '<CR>")
 map.vnoremap("<leader>c:", ":!lensort ': '<CR>")
