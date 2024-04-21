c800bc7ecc3fbb5f7f162d0ac60436099d590b29
barts <bartsteensma@outlook.com>
2024-04-21 21:05:13 +0200

    'Automatic Update'

--- dotfiles/nvim/lua/config/fzf.lua
+++ dotfiles/nvim/lua/config/fzf.lua
@@ -8,12 +8,43 @@ local function files_home()
     fzf.files({ cwd = os.getenv("HOME") })
 end
 
+-- list directories in current working directory and all subdirectories. Call 
+-- :e <directory> to open the selected directory in a new buffer.
+-- local function directories()
+-- end
+
 local function files_nvim_config()
     fzf.files({ cwd = join(os.getenv("HOME"), "dotfiles", "nvim") })
 end
 
+require("fzf-lua")
 
-local opts = {}
+local opts = {
+    winopts = {
+        height  = 0.9,
+        width   = 0.9,
+        preview = {
+            hidden = "hidden",
+        }
+    },
+    keymap  = {
+        builtin = {
+            ["<F2>"]     = "toggle-fullscreen",
+            ["<C-p>"]    = "toggle-preview",
+            ["<S-down>"] = "preview-page-down",
+            ["<S-up>"]   = "preview-page-up",
+        },
+        fzf = {
+            ["ctrl-d"]     = "half-page-down",
+            ["ctrl-u"]     = "half-page-up",
+            ["ctrl-a"]     = "beginning-of-line",
+            ["ctrl-e"]     = "end-of-line",
+            ["alt-a"]      = "toggle-all",
+            ["shift-down"] = "preview-page-down",
+            ["shift-up"]   = "preview-page-up",
+        },
+    },
+}
 
 M.setup = function()
     fzf.setup(opts)
@@ -22,6 +53,9 @@ M.setup = function()
     map.nnoremap("<a-h>", files_home)
     map.nnoremap("<a-v>", files_nvim_config)
     map.nnoremap("<a-H>", fzf.help_tags)
+    map.nnoremap("<a-a>", fzf.grep())
+    map.nnoremap("<a-A>", fzf.live_grep())
+    map.nnoremap("<a-B>", fzf.git_branches)
 end
 
 return M
--- dotfiles/nvim/lua/plugins/fzf.lua
+++ dotfiles/nvim/lua/plugins/fzf.lua
@@ -1,31 +1,31 @@
-return {
-    'junegunn/fzf.vim',
-    keys = {
-        { '<a-o>' },
-        { '<a-v>' },
-        { '<a-V' },
-        { '<a-r>' },
-        { '<a-c>' },
-        { '<a-h>' },
-        { '<a-H>' },
-        { '<a-a>' },
-        { '<a-b>' },
-        { '<a-B>' },
-    },
-    cmd = { 'Ag' },
-    config = function()
-        vim.cmd('runtime vim/config/fzf.vim')
-    end,
-    dependencies = {
-        { 'junegunn/fzf', build = ':call fzf#install()' },
-    }
-}
-
 -- return {
---   "ibhagwan/fzf-lua",
---   dependencies = { "nvim-tree/nvim-web-devicons" },
---   config = function()
---     -- calling `setup` is optional for customization
---     require("config.fzf").setup()
---   end
+--     'junegunn/fzf.vim',
+--     keys = {
+--         { '<a-o>' },
+--         { '<a-v>' },
+--         { '<a-V' },
+--         { '<a-r>' },
+--         { '<a-c>' },
+--         { '<a-h>' },
+--         { '<a-H>' },
+--         { '<a-a>' },
+--         { '<a-b>' },
+--         { '<a-B>' },
+--     },
+--     cmd = { 'Ag' },
+--     config = function()
+--         vim.cmd('runtime vim/config/fzf.vim')
+--     end,
+--     dependencies = {
+--         { 'junegunn/fzf', build = ':call fzf#install()' },
+--     }
 -- }
+
+return {
+  "ibhagwan/fzf-lua",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    -- calling `setup` is optional for customization
+    require("config.fzf").setup()
+  end
+}
