a7921792d55b4411d73e2d9ae37768c3956c40be
barts <b.steensma@fleetcleaner.com>
2024-04-23 08:43:59 +0200

    'Automatic Update'

---
+++ dotfiles/nvim/lua/config/web-devicons.lua
@@ -0,0 +1,14 @@
+require("nvim-web-devicons").setup({
+    override_by_extension = {
+        ["txt"] = { icon = "" },
+        ["svg"] = { icon = "" },
+        ["xml"] = { icon = "" },
+        ["xmll"] = { icon = "" },
+        ["xll"] = { icon = "" },
+        ["scm"] = { icon = "" },
+        ["smm"] = { icon = "" },
+        ["toml"] = { icon = "" },
+        ["docx"] = { icon = "" },
+        ["log"] = { icon = "" },
+    },
+})
--- dotfiles/nvim/lua/plugins/web-devicons.lua
+++ dotfiles/nvim/lua/plugins/web-devicons.lua
@@ -1,11 +1,6 @@
 return {
     "nvim-tree/nvim-web-devicons",
     config = function()
-        require("nvim-web-devicons").setup({
-            override_by_extension = {
-                ["txt"] = { icon = "?" },
-                ["svg"] = { icon = "?" },
-            },
-        })
+        require("config.web-devicons")
     end
 }
