bbccab10ccaf064768e0264f040598064b3fa389
barts <b.steensma@fleetcleaner.com>
2024-04-29 21:02:57 +0200

    'Automatic Update'

--- dotfiles/nvim/lua/plugins/fugitive.lua
+++ dotfiles/nvim/lua/plugins/fugitive.lua
@@ -1,9 +1,10 @@
 return {
     'tpope/vim-fugitive',
     keys = {
-        { '<a-g>', ':G ' },
-        { '<a-G>', ':vertical G<CR>' },
-        { '<a-d>', ':silent! normal gf<CR>:wincmd o<bar>Gvdiffsplit<CR>' }
+        { '<a-g>',    ':G ' },
+        { '<a-G>',    ':vertical G<CR>' },
+        { '<a-d>',    ':silent! normal gf<CR>:wincmd o<bar>Gvdiffsplit<CR>' },
+        { '<leader>d' }
     },
     config = function()
         vim.cmd('runtime vim/config/fugitive.vim')
--- dotfiles/nvim/lua/plugins/fzf.lua
+++ dotfiles/nvim/lua/plugins/fzf.lua
@@ -1,5 +1,7 @@
 return {
     "ibhagwan/fzf-lua",
+    keys = { "<a-o>", "<a-b>", "<a-h>", "<a-v>", "<a-H>", "<a-a>", "<a-A>",
+        "<a-B>", "<a-c>", "<a-r>", "<a-t>" },
     config = function()
         require("config.fzf")
     end,
