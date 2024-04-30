d4871693f56f24f6fe550cc8a5c13a84658a8617
barts <b.steensma@fleetcleaner.com>
2024-04-30 22:13:06 +0200

    'Automatic Update'

--- dotfiles/nvim/lua/config/lualine.lua
+++ dotfiles/nvim/lua/config/lualine.lua
@@ -1,9 +1,18 @@
 local lualine = require("lualine")
 local custom_auto = require('lualine.themes.auto')
 
-custom_auto.inactive.c.fg = '#BBBBBB'
 custom_auto.inactive.c.bg = nil
-custom_auto.inactive.c.gui = 'bold'
+custom_auto.inactive.c.gui = nil
+
+local function tabs()
+    local current_tab = vim.fn.tabpagenr()
+    local number_of_tabs = vim.fn.tabpagenr('$')
+    if number_of_tabs > 1 then
+        return string.format('%d/%d', current_tab, number_of_tabs)
+    else
+        return ''
+    end
+end
 
 lualine.setup({
     options = {
@@ -13,41 +22,30 @@ lualine.setup({
         component_separators = { left = '', right = '' },
         disabled_filetypes = {
             statusline = {},
-            winbar = {},
         },
         ignore_focus = {},
         always_divide_middle = false,
-        globalstatus = false,
+        globalstatus = true,
         refresh = {
             statusline = 1000,
-            tabline = 1000,
         }
     },
     sections = {
         lualine_a = { 'mode' },
         lualine_b = { 'branch', 'diff', 'diagnostics' },
-        lualine_c = { 'filename' },
-        lualine_x = { 'copilot', 'encoding', 'fileformat', 'filetype' },
-        lualine_y = { 'progress' },
-        lualine_z = { 'location' }
-    },
-    inactive_sections = {
-        lualine_a = {},
-        lualine_b = {},
-        lualine_c = { 'filename' },
-        lualine_x = {},
+        lualine_c = { 'copilot' },
+        lualine_x = {
+            {
+                'filename',
+                path = 4,
+            },
+            'filetype',
+            'fileformat',
+        },
         lualine_y = {},
-        lualine_z = {}
-    },
-    tabline = {
-        lualine_a = {},
-        lualine_b = {},
-        lualine_c = { 'filename' },
-        lualine_x = {},
-        lualine_y = {},
-        lualine_z = {}
+        lualine_z = { 'progress', tabs }
     },
     extensions = { 'fugitive', 'fzf', 'quickfix', "lazy", "mason", "oil" }
 })
 
-vim.opt.showtabline = 1
+vim.opt.showtabline = 0
