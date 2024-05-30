6728d5d222b5032bfacafec61d8769398c127ac4
barts <bartsteensma@outlook.com>
2024-05-30 22:11:18 +0200

    'Automatic Update'

--- .pdbrc.py
+++
@@ -1,9 +0,0 @@
-from pdb import DefaultConfig
-
-
-class Config(DefaultConfig):
-    prompt = "(Pdb++) "
-    highlight = True
-    pygments_style = "gruvbox"
-    use_pygments = True
-    sticky_by_default = True
