64da30ecfcd3904766a9d54eea4c0a075af6c137
bartsteensma <b.steensma@fleetcleaner.com>
2024-06-02 21:23:18 +0200

    Automatic update

--- dotfiles/nvim/lua/config/lsp/servers.lua
+++ dotfiles/nvim/lua/config/lsp/servers.lua
@@ -80,9 +80,11 @@ lsp.marksman.setup(get_marksman_opts())
 lsp.ruff_lsp.setup({})
 if vim.fn.has('win32') == 1 then
     local appdata = vim.fn.expand('$LOCALAPPDATA')
-    local pwsh_es_cmd = path.join(appdata, 'nvim-data', 'mason', 'packages', 'powershell-editor-services',
-        'PowerShellEditorServices', 'Start-EditorServices.ps1')
+    local bundle_path = path.join(appdata, 'nvim-data', 'mason', 'packages', 'powershell-editor-services',
+        'PowerShellEditorServices')
+    local cmd = path.join(bundle_path, 'Start-EditorServices.ps1')
     lsp.powershell_es.setup({
-        cmd = { 'pwsh', '-NoLogo', '-NoProfile', '-Command', pwsh_es_cmd },
+        bundle_path = bundle_path,
+        cmd = { 'pwsh', '-NoLogo', '-NoProfile', '-Command', cmd },
     })
 end
