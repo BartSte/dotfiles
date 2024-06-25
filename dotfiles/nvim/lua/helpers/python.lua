fdd40d3a97839e69f3bd81b86340c8d3efd6d516
barts <bartsteensma@outlook.com>
2024-06-25 22:45:04 +0200

    'Automatic Update'

--- dotfiles/nvim/lua/helpers/python.lua
+++ dotfiles/nvim/lua/helpers/python.lua
@@ -6,8 +6,8 @@ local function log(msg, level)
 end
 
 local function file_tree_is_valid()
-    local has_src = vim.fn.filereadable('src')
-    local has_tests = vim.fn.filereadable('tests')
+    local has_src = vim.fn.isdirectory('src')
+    local has_tests = vim.fn.isdirectory('tests')
     return has_src == 1 and has_tests == 1
 end
 
@@ -28,13 +28,16 @@ local function get_toggle_file()
     local is_src = vim.fn.match(rel_path, 'src/') ~= -1
     local is_test = vim.fn.match(rel_path, 'tests/') ~= -1
 
-    if is_src then
+    if file_tree_is_valid() == 0 then
+        log("Invalid file tree", vim.log.levels.ERROR)
+        return rel_path
+    elseif not is_src and not is_test then
+        log("Not in src or tests directory", vim.log.levels.ERROR)
+        return rel_path
+    elseif is_src then
         return get_test_file(rel_path)
     elseif is_test then
         return get_src_file(rel_path)
-    else
-        log("Not in src or tests directory", vim.log.levels.ERROR)
-        return rel_path
     end
 end
 
@@ -54,17 +57,7 @@ end
 ---     └── test_dir
 ---        └── test_file.py
 M.toggle_test_file = function()
-    if file_tree_is_valid() then
-        log("No src and/or tests directory found", vim.log.levels.ERROR)
-        return
-    end
-
-    local path = get_toggle_file()
-    if vim.fn.filereadable(path) == 0 then
-        log("File not found: " .. path, vim.log.levels.ERROR)
-    else
-        vim.cmd('edit ' .. path)
-    end
+    vim.cmd('edit ' .. get_toggle_file())
 end
 
 return M
