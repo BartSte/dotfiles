494bc5fd0877cc0b5bbe8510c228602ea41ee6a9
barts <b.steensma@fleetcleaner.com>
2024-09-03 08:48:52 +0200

    'Automatic Update'

--- dotfiles/nvim/lua/after/ftplugin/python/winpyproject.lua
+++ dotfiles/nvim/lua/after/ftplugin/python/winpyproject.lua
@@ -1,7 +1,18 @@
 local M = {}
 
 M.setup = function()
-    vim.opt_local.textwidth = 100
+    --Try to open the file `pyproject.toml` in the current directory.
+    --If it exists, extract the line that holds `max_line_length`.
+    --From this line, extract the value after the `=`.
+    --Use this value as the vim.opt_local.textwidth value.
+    local max_line_length = vim.fn.systemlist("cat pyproject.toml | grep max_line_length")
+    if #max_line_length > 0 then
+        local line = max_line_length[1]
+        local value = line:match("= (%d+)")
+        if value then
+            vim.opt_local.textwidth = tonumber(value)
+        end
+    end
 end
 
 return M
