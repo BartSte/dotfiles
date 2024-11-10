d0d6205f4e36d95b3817e12eab382929f72de0e3
barts <bartsteensma@outlook.com>
2024-11-10 21:11:23 +0100

    'Automatic Update'

--- dotfiles/nvim/lua/after/plugin/sorters.lua
+++ dotfiles/nvim/lua/after/plugin/sorters.lua
@@ -1,6 +1,23 @@
 local map = require("helpers.keymapper")
 
-map.nnoremap("<leader>cc", "vip:sort<CR>")
+
+-- TODO: read abount the `:map-operator` help page to implement an operator
+-- pending sorter.`
+--
+-- function _G.CmdOperator(type)
+--     if type == 'line' then
+--         vim.cmd('normal! `[V`]')
+--         vim.cmd("'[,']" .. cmd)
+--     elseif type == 'char' then
+--         vim.cmd('normal! `[v`]')
+--         vim.cmd("'[,']" .. cmd)
+--     else
+--         vim.notify("Blockwise sort not supported", vim.log.levels.ERROR)
+--     end
+-- end
+-- map.nnoremap("<leader>cc", ":<C-u>set opfunc=v:lua.SortOperator<CR>g@")
+
+map.nnoremap("<leader>c,", "vip:!lensort ','<CR>")
 map.vnoremap("<leader>cc", ":sort<CR>")
 
 map.nnoremap("<leader>c=", "vip:!lensort '='<CR>")
