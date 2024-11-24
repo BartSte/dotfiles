bd87e97a0d103e7c160046c811cef4a991702764
barts <bartsteensma@outlook.com>
2024-11-24 21:02:23 +0100

    'Automatic Update'

--- dotfiles/nvim/lua/plugins/fugitive.lua
+++ dotfiles/nvim/lua/plugins/fugitive.lua
@@ -1,8 +1,6 @@
 return {
     "tpope/vim-fugitive",
-    keys = {
-        { "<space>g", "<space>G"}
-    },
+    keys = { "<space>g", "<space>G" },
     config = function()
         vim.cmd("runtime vim/config/fugitive.vim")
     end
