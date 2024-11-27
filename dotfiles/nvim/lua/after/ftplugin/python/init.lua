90280ced381c4f391b2a86955bd7fd35159d4cc2
barts <bartsteensma@outlook.com>
2024-11-27 18:36:43 +0100

    'Automatic Update'

--- dotfiles/nvim/lua/after/ftplugin/python/init.lua
+++ dotfiles/nvim/lua/after/ftplugin/python/init.lua
@@ -8,12 +8,11 @@ map.buffer_nnoremap('gl', ':TestVisit<CR>')
 map.buffer_nnoremap('gs', helpers.toggle_test_file)
 map.buffer_nnoremap('gS', helpers.split_window_src_test)
 
-map.buffer_nnoremap('<a-f>', ':TestFile -strategy=pytesttmux<CR>')
-map.buffer_nnoremap('<a-l>', ':TestLast -strategy=pytesttmux<CR>')
-map.buffer_nnoremap('<a-t>', ':TestNearest -strategy=pytesttmux<CR>')
-
-map.buffer_nnoremap('<a-F>', ':TestFile -strategy=pytesttmuxdebug<CR>')
-map.buffer_nnoremap('<a-L>', ':TestLast -strategy=pytesttmuxdebug<CR>')
-map.buffer_nnoremap('<a-T>', ':TestNearest -strategy=pytesttmuxdebug<CR>')
+map.buffer_nnoremap('<leader>tf', ':TestFile -strategy=pytesttmux<CR>')
+map.buffer_nnoremap('<leader>tl', ':TestLast -strategy=pytesttmux<CR>')
+map.buffer_nnoremap('<leader>tt', ':TestNearest -strategy=pytesttmux<CR>')
+map.buffer_nnoremap('<leader>tF', ':TestFile -strategy=pytesttmuxdebug<CR>')
+map.buffer_nnoremap('<leader>tL', ':TestLast -strategy=pytesttmuxdebug<CR>')
+map.buffer_nnoremap('<leader>tT', ':TestNearest -strategy=pytesttmuxdebug<CR>')
 
 require("projectrc").require("after.ftplugin.python").setup()
