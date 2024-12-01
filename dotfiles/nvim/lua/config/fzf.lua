b49be0eb1e420988b94e0c9d4020ca4036f43193
barts <bartsteensma@outlook.com>
2024-12-01 22:10:50 +0100

    'Automatic Update'

--- dotfiles/nvim/lua/config/fzf.lua
+++ dotfiles/nvim/lua/config/fzf.lua
@@ -29,19 +29,14 @@ fzf.setup({
     },
 })
 
-map.nnoremap("<a-f>", fzf.files)
+map.nnoremap("<a-H>", fzf.help_tags)
 map.nnoremap("<a-b>", fzf.buffers)
 map.nnoremap("<a-d>", helpers.dirs)
-map.nnoremap("<a-e>", fzf.help_tags)
+map.nnoremap("<a-f>", fzf.files)
 map.nnoremap("<a-h>", helpers.files_home)
 map.nnoremap("<a-r>", helpers.files_org)
 map.nnoremap("<a-t>", fzf.tabs)
 map.nnoremap("<a-v>", helpers.files_nvim_config)
-
---The capital version of the keymap is used to search without .ignore.
-map.nnoremap("<a-F>", helpers.files_all)
-map.nnoremap("<a-D>", helpers.dirs_all)
-map.nnoremap("<a-H>", helpers.files_home_all)
 
 -- With grep seach, the capital rule does not apply.
 map.nnoremap("<a-A>", fzf.live_grep_resume)
