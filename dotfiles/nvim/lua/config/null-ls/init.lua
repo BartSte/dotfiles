5e6587036865803d4e3047b1e95366a2977ddb32
barts <bartsteensma@outlook.com>
2024-12-02 21:19:13 +0100

    'Automatic Update'

--- dotfiles/nvim/lua/config/null-ls/init.lua
+++ dotfiles/nvim/lua/config/null-ls/init.lua
@@ -1,33 +1,26 @@
-local M = {}
+local null_ls = require("null-ls")
 
-M.setup = function()
-    local null_ls = require("null-ls")
-
-    local sources = {
-        -- Python
-        require("helpers.null-ls.formatters").autoflake,
-        require("helpers.null-ls.formatters").isort,
-        -- C++
-        null_ls.builtins.formatting.cmake_format,
-        null_ls.builtins.diagnostics.cmake_lint,
-        -- Shell
-        null_ls.builtins.formatting.shfmt.with({
-            filetypes = { "sh", "bash", "zsh" },
-            args = { "-i", "4" },
-        }),
-        -- Markdown
-        null_ls.builtins.formatting.prettierd.with({
-            extra_filetypes = { "toml" },
-        }),
-        null_ls.builtins.diagnostics.markdownlint.with({
-            args = { "--stdin", "--disable", "MD013" },
-        })
-    }
-    local opts = { fallback_value = {} }
-    local sources_project = require("projectrc").require("config.null-ls", opts)
-    vim.list_extend(sources, sources_project)
-    null_ls.setup({ sources = sources })
-end
-
-
-return M
+local sources = {
+    -- Python
+    require("helpers.null-ls.formatters").autoflake,
+    require("helpers.null-ls.formatters").isort,
+    -- C++
+    null_ls.builtins.formatting.cmake_format,
+    null_ls.builtins.diagnostics.cmake_lint,
+    -- Shell
+    null_ls.builtins.formatting.shfmt.with({
+        filetypes = { "sh", "bash", "zsh" },
+        args = { "-i", "4" },
+    }),
+    -- Markdown
+    null_ls.builtins.formatting.prettierd.with({
+        extra_filetypes = { "toml" },
+    }),
+    null_ls.builtins.diagnostics.markdownlint.with({
+        args = { "--stdin", "--disable", "MD013" },
+    })
+}
+local opts = { fallback_value = {} }
+local sources_project = require("projectrc").require("config.null-ls", opts)
+vim.list_extend(sources, sources_project)
+null_ls.setup({ sources = sources, debug = true })
