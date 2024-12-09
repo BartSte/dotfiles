cc92e1e4b003be0cf8009f93758a440cde6016a7
barts <b.steensma@fleetcleaner.com>
2024-12-09 21:07:34 +0100

    'Automatic Update'

--- dotfiles/nvim/lua/config/lsp/filters/winpyproject.lua
+++ dotfiles/nvim/lua/config/lsp/filters/winpyproject.lua
@@ -3,9 +3,8 @@ local M = {}
 --- The formatter of ruff is not allowed.
 ---@param client table lsp client
 ---@return boolean result true: allow, false: not allow
-M.filter = function(client)
+M.format = function(client)
     local is_ruff = client.name == "ruff"
-    -- dd("is_ruff", is_ruff)
     return not is_ruff
 end
 
