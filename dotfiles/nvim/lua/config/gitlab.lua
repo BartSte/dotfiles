bec37dbbebbff68832d9d9b747c8ad9245467bfb
barts <bartsteensma@outlook.com>
2025-02-22 19:58:11 +0100

    'Automatic Update'

--- dotfiles/nvim/lua/config/gitlab.lua
+++ dotfiles/nvim/lua/config/gitlab.lua
@@ -1,7 +1,6 @@
 local commands          = require("gitlab.commands")
 local defaults          = require("gitlab.config.defaults")
 local gitlab            = require("gitlab")
-local mapper            = require("helpers.keymapper")
 
 local default_filetypes = defaults.code_suggestions.auto_filetypes
 local auto_filetypes    = { "lua", "markdown", "zsh", "sh", "toml", "yaml" }
@@ -15,6 +14,10 @@ gitlab.setup({
       toggle_enabled = false,
       accept_suggestion = '<Right>',
       clear_suggestions = '<Left>',
+      insert_word = "<C-Right>",
+      restore_word = "<C-Left>",
+      insert_line = "<S-Right>",
+      restore_line = "<S-Left>",
       stream = false,
     },
   },
@@ -26,5 +29,3 @@ gitlab.setup({
   },
 })
 commands.create() -- Needed to reset the `FileType` autocommand.
-mapper.inoremap("<C-Right>", require("gitlab.ghost_text").insert_word)
-mapper.inoremap("<S-Right>", require("gitlab.ghost_text").insert_line)
--- dotfiles/nvim/lua/plugins/gitlab.lua
+++ dotfiles/nvim/lua/plugins/gitlab.lua
@@ -1,9 +1,9 @@
 return {
-    "BartSte/gitlab.vim",
-    cond = function()
-        return vim.env.GITLAB_TOKEN ~= nil and vim.env.GITLAB_TOKEN ~= ''
-    end,
-    config = function()
-        require("config.gitlab")
-    end
+  "BartSte/gitlab.vim",
+  cond = function()
+    return vim.env.GITLAB_TOKEN ~= nil and vim.env.GITLAB_TOKEN ~= ''
+  end,
+  config = function()
+    require("config.gitlab")
+  end
 }
