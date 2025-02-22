29289c9d2c682d169b6ace6ec16a37dc5142526d
barts <bartsteensma@outlook.com>
2025-02-22 20:55:29 +0100

    'Automatic Update'

--- dotfiles/nvim/lua/config/gitlab.lua
+++ dotfiles/nvim/lua/config/gitlab.lua
@@ -3,7 +3,7 @@ local defaults          = require("gitlab.config.defaults")
 local gitlab            = require("gitlab")
 
 local default_filetypes = defaults.code_suggestions.auto_filetypes
-local auto_filetypes    = { "lua", "markdown", "zsh", "sh", "toml", "yaml" }
+local auto_filetypes    = { "lua", "markdown", "zsh", "sh", "bash", "toml", "yaml" }
 vim.list_extend(auto_filetypes, default_filetypes)
 
 gitlab.setup({
