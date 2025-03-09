f7de6eef98d854eda454153b34cc1c8bfd65c568
barts <bartsteensma@outlook.com>
2025-03-09 17:21:32 +0100

    'Automatic Update'

--- dotfiles/nvim/lua/config/lsp/servers.lua
+++ dotfiles/nvim/lua/config/lsp/servers.lua
@@ -1,83 +1,54 @@
 return {
-  basedpyright = {
-    settings = {
-      basedpyright = {
-        disableOrganizeImports = true,
-      },
-      python = {
-        analysis = {
-          autoSearchPaths = false,
-          diagnosticMode = "openFilesOnly",
-          useLibraryCodeForTypes = false,
-          exclude = { "venv", ".git", "build", "dist" },
-          diagnosticSeverityOverrides = {
-            reportUnusedCallResult = false,
-          },
+    basedpyright = {
+        settings = {
+            basedpyright = {
+                disableOrganizeImports = true,
+            },
+            python = {
+                analysis = {
+                    autoSearchPaths = false,
+                    diagnosticMode = "openFilesOnly",
+                    useLibraryCodeForTypes = false,
+                    exclude = { "venv", ".git", "build", "dist" },
+                    diagnosticSeverityOverrides = {
+                        reportUnusedCallResult = false,
+                    },
+                }
+            },
+        },
+    },
+
+    clangd = {
+        cmd = {
+            "clangd",
+            "--offset-encoding=utf-16",
+            "--header-insertion=iwyu",
+            "--background-index",
+            "--clang-tidy",
+            "--pch-storage=memory",
+        },
+    },
+
+    bashls = { filetypes = { "sh", "bash", "zsh" } },
+
+    lua_ls = {
+        settings = {
+            Lua = {
+                workspace = {
+                    library = vim.api.nvim_get_runtime_file("lua", true),
+                }
+            }
         }
-      },
     },
-  },
 
-  clangd = {
-    cmd = {
-      "clangd",
-      "--offset-encoding=utf-16",
-      "--header-insertion=iwyu",
-      "--background-index",
-      "--clang-tidy",
-      "--pch-storage=memory",
+    ruff = {
+        extra_args = { "--select", "F401", "--fix" }
     },
-  },
-
-  bashls = { filetypes = { "sh", "bash", "zsh" } },
-
-  lua_ls = {
-    settings = {
-      Lua = {
-        runtime = {
-          version = 'LuaJIT'
-        },
-        completion = {
-          autoRequire = true,
-        },
-        format = {
-          enable = true,
-          defaultConfig = {
-            line_length = 100,
-            indent_style = 'space',
-            indent_size = 2,
-          }
-        },
-        diagnostics = {
-          globals = { 'vim', 'Snacks' },
-          neededFileStatus = {
-            ['codestyle.long_line'] = 'Any',
-          },
-          groupSeverity = {
-            strong = 'Warning',
-          },
-          groupFileStatus = {
-            ['codestyle'] = 'Opened',
-          },
-        },
-        workspace = {
-          maxPreload = 2000,
-          preloadFileSize = 500,
-          checkThirdParty = false,
-          library = vim.api.nvim_get_runtime_file("lua", true)
-        },
-      }
-    }
-  },
-
-  ruff = {
-    extra_args = { "--select", "F401", "--fix" }
-  },
-  cmake = {},
-  vimls = {},
-  marksman = {},
-  jsonls = {},
-  ts_ls = {},
-  gitlab_ci_ls = {},
-  yamlls = {}
+    cmake = {},
+    vimls = {},
+    marksman = {},
+    jsonls = {},
+    ts_ls = {},
+    gitlab_ci_ls = {},
+    yamlls = {}
 }
