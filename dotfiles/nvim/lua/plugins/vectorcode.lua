63b20df1429be2d9ff46ba6943d10c90afc1372c
barts <bartsteensma@outlook.com>
2025-03-30 21:45:32 +0200

    'Automatic Update'

--- dotfiles/nvim/lua/plugins/vectorcode.lua
+++ dotfiles/nvim/lua/plugins/vectorcode.lua
@@ -1,5 +1,6 @@
 return {
     "Davidyz/VectorCode",
+    cond = false,
     version = "*",                     -- optional, depending on whether you're on nightly or release
     build = "pipx upgrade vectorcode", -- optional but recommended if you set `version = "*"`
     dependencies = { "nvim-lua/plenary.nvim" },
