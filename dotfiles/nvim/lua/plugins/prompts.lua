5d76ee5eb8caad3f44354b1006d6dd3b1d8e6619
barts <bartsteensma@outlook.com>
2025-04-13 21:43:21 +0200

    'Automatic Update'

--- dotfiles/nvim/lua/plugins/prompts.lua
+++ dotfiles/nvim/lua/plugins/prompts.lua
@@ -1,6 +1,9 @@
 return {
     -- "BartSte/nvim-bartste-prompts",
-    dir="~/code/nvim-bartste-prompts",
+    dir = "~/code/nvim-bartste-prompts",
+    --- Initialize and configure the prompts plugin
+    -- @param plugin PluginSpec The plugin specification table
+    -- @return nil
     config = function()
         require("config.prompts")
     end
