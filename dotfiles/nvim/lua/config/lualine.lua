18a07628943003b1821adbb7bbcb7861d3fb7b55
barts <bartsteensma@outlook.com>
2025-04-23 17:14:25 +0200

    'Automatic Update'

--- dotfiles/nvim/lua/config/lualine.lua
+++ dotfiles/nvim/lua/config/lualine.lua
@@ -83,7 +83,7 @@ lualine.setup({
         lualine_a = { "mode" },
 
         -- Same for all files
-        lualine_b = { gitlab, helpers.codecompanion.spinner, projectmarks.shada, projectrc, venv },
+        lualine_b = { gitlab, helpers.codecompanion.spinner, projectmarks.shada, projectrc, venv, require("prompts.lualine") },
 
         -- Changes when switching buffers
         lualine_c = { "branch", "diff", "diagnostics", projectmarks.marks_optimized },
