2e20ea9d84bb28d2f1bbb4754e0950eddc24f55c
barts <bartsteensma@outlook.com>
2025-05-14 21:24:37 +0200

    'Automatic Update'

--- dotfiles/nvim/lua/config/prompts.lua
+++ dotfiles/nvim/lua/config/prompts.lua
@@ -4,18 +4,17 @@ local mapper = require("helpers.keymapper")
 --- Sets API keys and model selection depending on current system
 ---@side effect Modifies vim.env with API keys and model selection
 local function setup_env()
-    local rbw = require("helpers.rbw")
     local hostname = vim.fn.hostname()
     if hostname == "bart-asus" then
-        rbw.set_env("DEEPSEEK_API_KEY")
+        vim.schedule(function() require("helpers.rbw").set_env("DEEPSEEK_API_KEY") end)
         vim.env["AIDER_MODEL"] = "deepseek/deepseek-reasoner"
     elseif hostname == "zbook" then
-        rbw.set_env("OPENAI_API_KEY")
+        vim.schedule(function() require("helpers.rbw").set_env("OPENAI_API_KEY") end)
         vim.env["AIDER_MODEL"] = "o4-mini"
     end
 end
 
-setup_env()
+setup_env() -- TODO: this one is slow due to rbw?
 require("prompts").setup({
     notify = true,
     loglevel = "INFO"
