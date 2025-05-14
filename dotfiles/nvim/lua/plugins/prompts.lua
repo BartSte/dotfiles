9105af55a74b442effda3c3c575c284fe65472e5
barts <bartsteensma@outlook.com>
2025-05-14 09:49:35 +0200

    'Automatic Update'

--- dotfiles/nvim/lua/plugins/prompts.lua
+++ dotfiles/nvim/lua/plugins/prompts.lua
@@ -4,6 +4,7 @@ return {
     --- Initialize and configure the prompts plugin
     -- @param plugin PluginSpec The plugin specification table
     -- @return nil
+    event = { "BufReadPre", "BufNewFile" },
     config = function()
         require("config.prompts")
     end
