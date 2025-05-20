836413a3bd024d653863407498735f7114cace3d
barts <b.steensma@fleetcleaner.com>
2025-05-20 14:16:24 +0200

    'Automatic Update'

--- dotfiles/nvim/lua/config/lsp/init.lua
+++ dotfiles/nvim/lua/config/lsp/init.lua
@@ -1,7 +1,10 @@
-local helpers = require("helpers.lsp")
-local mappings = require('config.lsp.mappings')
-local server_opts = require('config.lsp.servers')
 local capabilities = require('config.lsp.capabilities')
+local fn           = require("helpers.fn")
+local helpers      = require("helpers.lsp")
+local lspconfig    = require("lspconfig")
+local mappings     = require('config.lsp.mappings')
+local server_opts  = require('config.lsp.servers')
+local util         = require("lspconfig.util")
 
 vim.diagnostic.config({
     virtual_text = false,
@@ -12,9 +15,27 @@ vim.diagnostic.config({
     float = { border = 'rounded' },
 })
 
+--- Wraps and decorates the root_dir function or path.
+---@param root_dir string|function Original root directory string or function.
+---@return function A function that resolves the root directory for a given buffer.
+local function decorate_root_dir(root_dir)
+    return function(bufnr, callback)
+    local fname = vim.api.nvim_buf_get_name(bufnr)
+    if fname:sub(1, 11) == "fugitive://" then
+        return nil
+    end
+    if type(root_dir) == "function" then
+        return root_dir(bufnr, callback)
+    end
+    return root_dir
+    end
+end
+
+
 ---The on_attach function can be used to setup configurations that are specific
 ---to a server. Server capabilities can be specified in the capabilities module.
 for name, config in pairs(server_opts) do
+    config.root_dir = decorate_root_dir(config.root_dir)
     vim.lsp.config(name, config)
     vim.lsp.enable(name)
 end
@@ -23,6 +44,8 @@ end
 ---used because the latter is triggered for each new buffer, which is not the
 ---case for the former.
 vim.api.nvim_create_autocmd('LspAttach', {
+    --- Handler for the LspAttach event. Updates client capabilities and mappings.
+    ---@param ev table Event data from Neovim, containing `client_id` and `buf`.
     callback = function(ev)
         local client = vim.lsp.get_client_by_id(ev.data.client_id)
         if not client or client == nil then
