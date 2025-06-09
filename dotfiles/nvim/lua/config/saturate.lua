b7a692d02f7e305b8e6e1e667ed551d392fbed47
barts <bartsteensma@outlook.com>
2025-06-09 07:37:46 +0200

    'Automatic Update'

--- dotfiles/nvim/lua/config/saturate.lua
+++ dotfiles/nvim/lua/config/saturate.lua
@@ -1,5 +1,6 @@
 require("saturate").setup({
     saturation = 3,
+    light_delta = 0.08,
     palette = require("kanagawa.colors").setup({ theme = "dragon" }).palette,
     after = function(palette)
         require("kanagawa").setup({ colors = { palette = palette } })
