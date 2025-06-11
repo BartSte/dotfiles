6aa1d2201ceaa1b4292dc3d489ebc7b5d0fa0e4a
barts <b.steensma@fleetcleaner.com>
2025-06-11 19:07:03 +0200

    'Automatic Update'

--- dotfiles/nvim/lua/config/saturate.lua
+++ dotfiles/nvim/lua/config/saturate.lua
@@ -1,10 +1,13 @@
+local function after(palette)
+    require("kanagawa").setup({ colors = { palette = palette } })
+    require("kanagawa").load()
+    vim.cmd("colorscheme kanagawa")
+end
+
 require("saturate").setup({
     saturation = 2,
     light_delta = 0.06,
     palette = require("kanagawa.colors").setup({ theme = "dragon" }).palette,
-    after = function(palette)
-        require("kanagawa").setup({ colors = { palette = palette } })
-        require("kanagawa").load()
-        vim.cmd("colorscheme kanagawa")
-    end
+    after = after,
+    log_level = vim.log.levels.WARN,
 })
