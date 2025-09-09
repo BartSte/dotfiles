c11a5d0d1b890729a0844a70910a4b6f4b8b4773
barts <b.steensma@fleetrobotics.com>
2025-09-09 21:53:47 +0200

    'Automatic Update'

--- dotfiles/nvim/lua/after/ftplugin/org.lua
+++ dotfiles/nvim/lua/after/ftplugin/org.lua
@@ -3,8 +3,9 @@ local keymapper = require('helpers.keymapper')
 vim.opt_local.spell = true
 vim.opt_local.shiftwidth = 2
 vim.opt_local.softtabstop = 2
-vim.opt_local.textwidth = 100
+vim.opt_local.textwidth = 0
 vim.opt_local.colorcolumn = '100'
+vim.opt_local.wrap = true
 
 keymapper.nnoremap('<leader>oie', ": lua require('config.fzf').get_email()<CR>")
 keymapper.nnoremap('<leader>f', ':normal! gg=G<CR><C-o>')
--- dotfiles/nvim/vim/config/table.vim
+++ dotfiles/nvim/vim/config/table.vim
@@ -1,1 +1,2 @@
 let g:table_mode_corner='|'
+noremap <leader>te :TableEvalFormulaLine<CR>
