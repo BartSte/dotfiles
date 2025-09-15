ecf518aeb3e4460874ecbf9a30d28cacf17be829
barts <b.steensma@fleetrobotics.com>
2025-09-15 10:38:33 +0200

    'Automatic Update'

--- dotfiles/nvim/lua/after/ftplugin/markdown.lua
+++ dotfiles/nvim/lua/after/ftplugin/markdown.lua
@@ -1,3 +1,4 @@
+let b:table_mode_corner='|'
 vim.opt_local.spell = true
 vim.opt_local.shiftwidth = 2
 vim.opt_local.softtabstop = 2
--- dotfiles/nvim/vim/config/table.vim
+++ dotfiles/nvim/vim/config/table.vim
@@ -1,2 +1,1 @@
-let g:table_mode_corner='|'
 noremap <leader>te :TableEvalFormulaLine<CR>
