df96c21c9e7814cc663c96d375a153f58706f44e
barts <b.steensma@fleetrobotics.com>
2025-10-27 12:06:43 +0100

    'Automatic Update'

--- dotfiles/nvim/lua/plugins/gitlab.lua
+++
@@ -1,8 +0,0 @@
-return {
-    -- "https://gitlab.com/gitlab-org/editor-extensions/gitlab.vim",
-    "BartSte/gitlab.vim",
-    -- dir = "~/code/gitlab.vim",
-    config = function()
-        require("config.gitlab")
-    end
-}
