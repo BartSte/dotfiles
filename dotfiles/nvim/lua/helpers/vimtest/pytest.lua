50d0e744764669803cb49575d377f4b591d9f583
bartste <b.steensma@fleetrobotics.com>
2026-01-10 12:44:05 +0100

    'Automatic Update'

--- dotfiles/nvim/lua/helpers/vimtest/pytest.lua
+++ dotfiles/nvim/lua/helpers/vimtest/pytest.lua
@@ -2,30 +2,6 @@ local tslime = require('helpers.tslime')
 
 local M = {}
 
--- Returns the output of `wpy -wpy-path` if it exists, otherwise returns ''.
-local function use_wuv()
-    return vim.env.WIN_VENV ~= nil and vim.fn.executable('wpy') == 1
-end
-
--- When running WSL and a venv is returned by `wpy -p`, use that venv. If not,
--- return the $VIRTUAL_ENV that corresponds to the OS.
-local function make_cmd()
-    if use_wuv() then
-        return "wuv run --with ipdb pytest "
-    else
-        return "uv run --with ipdb pytest "
-    end
-end
-
--- Replace the first occurence of `pattern` in `cmd` with `replacement`. If
--- `replacement` is '' or nil, return `cmd` unchanged.
-local function replace(cmd, pattern, replacement)
-    if replacement == '' or replacement == nil then
-        return cmd
-    else
-        return string.gsub(cmd, pattern, replacement, 1)
-    end
-end
 
 local default_opts = {
     prefix = 'tmux clearhist; clear;',
@@ -43,12 +19,15 @@ M.make_strategy = function(opts)
     return function(cmd)
         if vim.fn.has('win32') == 1 then
             vim.notify('pytest.lua is not supported on Windows. Use WSL instead.', vim.log.levels.WARN)
+        elseif vim.fn.executable('wuv') == 0 then
+            vim.notify('wuv is not installed. Please install wuv from the wslpy project.', vim.log.levels.WARN)
         else
+            local wuv = "wuv --wuv-fallback run --with ipdb pytest "
             local args = opts.args .. ' --log-level=' .. opts.loglevel
 
-            cmd = replace(cmd, "^python[^ ]+", make_cmd())
-            cmd = replace(cmd, "uv run pytest", make_cmd())
-            cmd = opts.prefix .. ' ' .. cmd .. ' ' .. args
+            cmd = string.gsub(cmd, "^python[^ ]*", wuv)
+            cmd = string.gsub(cmd, "^uv run pytest", wuv)
+            cmd = string.format("%s %s %s", opts.prefix, cmd, args)
 
             tslime.send_to_tmux(cmd .. '\n')
         end
