314ddfc6c85b0755fe7c41b031f19de74f60b6b6
BartSte <>
2026-02-12 12:51:13 +0000

    'Automatic Update'

--- dotfiles/nvim/lua/plugins/prompts.lua
+++ dotfiles/nvim/lua/plugins/prompts.lua
@@ -1,12 +1,12 @@
 return {
-    "BartSte/nvim-bartste-prompts",
-    -- dir = "~/code/personal/lua/nvim-bartste-prompts",
-    --- Initialize and configure the prompts plugin
-    -- @param plugin PluginSpec The plugin specification table
-    -- @return nil
-    cond = require("helpers.platform").is_linux(),
-    event = { "BufReadPre", "BufNewFile" },
-    config = function()
-        require("config.prompts")
-    end
+    -- "BartSte/nvim-bartste-prompts",
+    -- -- dir = "~/code/personal/lua/nvim-bartste-prompts",
+    -- --- Initialize and configure the prompts plugin
+    -- -- @param plugin PluginSpec The plugin specification table
+    -- -- @return nil
+    -- cond = require("helpers.platform").is_linux(),
+    -- event = { "BufReadPre", "BufNewFile" },
+    -- config = function()
+    --     require("config.prompts")
+    -- end
 }
